87ed629cfae0a4ddda9ae8dcf1a0f0306fe908f5
CeronMoa <56114106+ceron999@users.noreply.github.com>
2024-08-21 18:46:22 +0900

    11
    11

--- Assets/Script/Study/InputSystem.cs
+++ Assets/Script/Study/InputSystem.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class InputSystem : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,18 +24,22 @@ public class InputSystem : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             //¾Õ
+            transform.position += transform.forward * speed * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.A))
         {
             //¿Þ
+            transform.position += transform.right * (-1) * speed * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.S))
         {
             //µÚ
+            transform.position += transform.forward * (-1) * speed * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.D))
         {
             //¿À
+            transform.position += transform.right * speed * Time.deltaTime;
         }
     }
     #endregion
